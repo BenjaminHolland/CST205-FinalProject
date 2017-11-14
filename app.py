7a67efd4a9eca95ebb4f5e3d1a902b84222f2cd5
Benjamin Holland <ben.holland.core@gmail.com>
2017-11-14 10:49:33 -0800

    Added modules for processing steps.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template,send_from_directory
 from flask_bootstrap import Bootstrap
+import track_step
+import audio_step
+import merge_step
+
 app=Flask(__name__)
 Bootstrap(app)
 
