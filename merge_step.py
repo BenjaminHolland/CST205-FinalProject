7f8a623bbf89afaedc1b07dc48cc9e1631a3f3df
Benjamin Holland <ben.holland.core@gmail.com>
2017-11-15 10:52:43 -0800

    Added first try at video merge step. Basic structure from https://superuser.com/questions/277642/how-to-merge-audio-and-video-file-in-ffmpeg

--- merge_step.py
+++ merge_step.py
@@ -1,3 +1,17 @@
+import ffmpeg
 """This step takes the generated audio data and the original video, and merges them together, outputting the final video"""
 def run(audio,video):
+    local_audio=_convert_audio(audio)
+    local_video=_convert_video(video)
+    result=ffmpeg
+        .input(local_video,c='copy')
+        .input(local_audio,c='aac')
+        .output('test.mp4')
+        
+    
+    return 'test.mp4'
     pass
+def _convert_audio(audio):
+    return None
+def _convert_video(video):
+    return None
