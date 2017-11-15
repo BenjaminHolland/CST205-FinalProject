c088e9e7ae63f1fb70912354c41a013dc8a86997
jmartineau <jmartineau@csumb.edu>
2017-11-15 11:44:25 -0800

    Moved video and audio files to static folder

--- track_step.py
+++ track_step.py
@@ -1,3 +1,14 @@
 """This module provides functionality that turns the original video into tracking information"""
+
+import numpy as np
+import cv2
+
+# Define cascade to track object
+casc_class = "haarcascade_frontalface_default.xml"
+red_cascade = cv2.CascadeClassifier(casc_class)
+
+# Define video file (For testing, will get passed in from app.py)
+
+
 def run(video):
     pass
