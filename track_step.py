830b8f75080733243b20996cb25b7f0d050f2eb5
jmartineau <jmartineau@csumb.edu>
2017-11-27 15:05:01 -0800

    Defined HSV color range for red ball

--- track_step.py
+++ track_step.py
@@ -1,14 +1,23 @@
 """This module provides functionality that turns the original video into tracking information"""
+""" Help from: https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/ """
 
 import numpy as np
 import cv2
 
-# Define cascade to track object
-casc_class = "haarcascade_frontalface_default.xml"
-red_cascade = cv2.CascadeClassifier(casc_class)
+# Define the lower and upper boundaries of the red
+# ball in the HSV color space
+redLower = (0, 100, 100)
+redUpper = (10, 255, 255)
 
 # Define video file (For testing, will get passed in from app.py)
-
+video = cv2.VideoCapture('static/VideoTest1.mp4')
 
 def run(video):
-    pass
+    while True:
+        returned, frame = video.read()
+
+        # Once the video has not returned a frame, the video is over
+        if returned == 0:
+            break
+
+run(video)
