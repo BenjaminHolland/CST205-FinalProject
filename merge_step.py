6c69f56f384ef1af15ced1dc5046f460759b258a
Benjamin Holland <ben.holland.core@gmail.com>
2017-11-27 09:54:15 -0800

    First Try

--- merge_step.py
+++ merge_step.py
@@ -1,17 +1,26 @@
 import ffmpeg
 """This step takes the generated audio data and the original video, and merges them together, outputting the final video"""
+
 def run(audio,video):
     local_audio=_convert_audio(audio)
     local_video=_convert_video(video)
-    result=ffmpeg
-        .input(local_video,c='copy')
-        .input(local_audio,c='aac')
-        .output('test.mp4')
-        
-    
+    result=ffmpeg.input(local_video,c='copy').input(local_audio,c='aac').output('test.mp4')
     return 'test.mp4'
-    pass
+def _test():
+    audio_file='TestAudio1.wav'
+    video_file='VideoTest1.mp4'
+    
+    (ffmpeg
+    
+        .input(video_file,c='copy')
+        .input(audio_file,c='aac')
+        .output('test.mp4')
+        .run())
+
 def _convert_audio(audio):
     return None
 def _convert_video(video):
     return None
+
+if(__name__=='__main__'):
+    _test()
